f3849d12d0ce4354b2e962abb9e26b20d5f1a6e8
aengun <aengun@icloud.com>
2021-04-10 23:25:03 +0900

    class

---
+++ 8.class/class.js
@@ -0,0 +1,15 @@
+class Person{
+
+}
+
+var kim = new Person();
+console.log(kim);
+
+// var kim = new Person('kim', 10, 20);
+// kim.sum = function () {
+//     return 'this : ' + (this.first + this.second);
+// }
+// var lee = new Person('lee', 10, 10);
+
+// console.log("kim.sum()", kim.sum());
+// console.log("lee.sum()", lee.sum());
